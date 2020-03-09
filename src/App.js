39b9f76887e45de61293f3f8ed6ba0dc095cbdda
Iizuki <36295229+Iizuki@users.noreply.github.com>
2020-03-09 11:29:11 +0200

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Example Name' }
+      ],
+      newName: ''
+    }
+  }
+
+  //Form even thandler keeping the newName state up to date.
+  handleNewPerson = (event) => {
+    this.setState({newName: event.target.value})
+  }
+
+  //Form event handle adding a new name to the list.
+  addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: this.state.newName
+    }
+    const nextPersons = this.state.persons.concat(newPerson)
+
+    this.setState({
+      persons: nextPersons,
+      newName: ''
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h2>Directory</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            name: <input 
+            value={this.state.newName}
+            onChange={this.handleNewPerson}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <Persons personList={this.state.persons}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+//A small component that arranges the list of persons to jsx.
+const Persons = ({personList}) => {
+  const rows = personList.map(person => <p key={person.name}>{person.name}</p>)
+    return (
+      <ul>
+        {rows}
+      </ul> 
+    )
+}
+
+export default App
+
